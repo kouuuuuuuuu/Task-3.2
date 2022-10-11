f7c4461cb3321e5c60309d2bb36133348f3e9b54
NguyenThinh <koulse.kudaza@gmail.com>
2022-10-11 16:40:06 +0700

    first commit

---
+++ models/user.py
@@ -0,0 +1,24 @@
+from odoo import api, fields, models, tools, SUPERUSER_ID, _
+
+
+# class ProjectStudy2(models.Model):
+#     _inherit = 'res.users'
+#     name = fields.Many2one('project.study')
+
+class ProjectStudy(models.Model):
+    _name = 'project.study'
+
+    name = fields.Char(required=True)
+    dateline = fields.Date(string="DateLine", required=True)
+    assign_to = fields.Many2one('res.users', string='Assigned To', default=lambda self: self.env.user)
+    note = fields.Text(string='Note')
+    description = fields.Html(string='Description')
+    list = [
+        ('TODO', 'TODO'),
+        ('IN - PROGRESS', 'IN - PROGRESS'),
+        ('REVIEW', 'REVIEW'),
+        ('REVIEW', 'DONE')
+    ]
+    status = fields.Selection(selection=list, string='Status', default='TODO')
+    project_managers = fields.Many2many('res.users', "user_working_rel_many2many", string='Project Managers')
+    task_attendees_ids = fields.Many2one('res.users', string='Task Attendees')
